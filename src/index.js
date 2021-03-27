030f90c25cacdf0b4195407514c832ec8872655d
ACarrill42 <austincarrill@gmail.com>
2021-03-26 20:52:55 -0500

    set up an empty reducer

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import {reducer} from './reducers/reducer'
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
-const store = createStore(/* reducer */)
+const store = createStore(reducer)
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<Provider> <App /> </Provider>, rootElement);
+ReactDOM.render(<Provider store={store}> <App /> </Provider>, rootElement);
